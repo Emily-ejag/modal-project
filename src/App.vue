0268280a858a8a2257044a75a103c08d699c0a7e
Emily Arteaga <62304493+Emily-ejag@users.noreply.github.com>
2023-11-14 17:07:33 -0800

    creating my practice files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{title}}</h1>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">click me</button> -->
+  <Modal :header="header" 
+    :text="text" theme="sale"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Modal
+  },
+  data(){
+    return{
+      title: 'A vue app :)',
+      header:'Sign up for giveaway',
+      text:'Gab your Nike bucket hat',
+
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
